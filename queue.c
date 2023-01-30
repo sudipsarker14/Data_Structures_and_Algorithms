c2cc10e59403c35f458854d11947cb38d823c625
sudipsarker14 <113766948+sudipsarker14@users.noreply.github.com>
2023-01-31 00:11:42 +0600

    Update queue.c

--- queue.c
+++ queue.c
@@ -3,36 +3,36 @@
 
 int arr[n], front = -1, rear = -1;
 
-void enQueue(int val) {
-  if (rear == n - 1)
+void enQueue(int data) {
+  if (rear == n - 1){
     printf("Queue is Full\n");
-  else {
+  } else {
     if (front == -1)
       front = 0;
     rear++;
-    arr[rear] = val;
-    printf("\nInserted: %d", val);
+    arr[rear] = data;
+    printf("Inserted: %d", data);
   }
 }
 
 void deQueue() {
-  if (front == -1)
-    printf("\nQueue is Empty\n");
-  else {
-    printf("\nDeleted: %d", arr[front]);
+  if (front == -1){
+    printf("Queue is Empty\n");
+  } else {
+    printf("Deleted: %d", arr[front]);
     front++;
-    if (front > rear)
+    if (front > rear){
       front = rear = -1;
+    } 
   }
 }
 
 void display() {
-  if (rear == -1)
-    printf("\nQueue is Empty!!!");
-  else {
-    int i;
-    printf("\nQueue elements are:\n");
-    for (i = front; i <= rear; i++)
+  if (rear == -1){
+    printf("Queue is Empty!!!");
+  } else {
+    printf("Queue elements are:\n");
+    for (int i = front; i <= rear; i++)
       printf("%d  ", arr[i]);
   }
   printf("\n");
