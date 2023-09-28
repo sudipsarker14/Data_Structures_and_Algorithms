a7604b8725670714b50e1daab5374de0ef3851e7
Sudip Sarker <113766948+sudipsarker14@users.noreply.github.com>
2023-09-28 15:33:22 +0600

    Adjacency Matrix Weighted Graph

--- adjMatWeight.cpp
+++ adjMatWeight.cpp
@@ -3,39 +3,37 @@
 
 using namespace std;
 
-int main()
-{
-    int totalnode, node1, node2, weight;
+int main(){
 
-    int mat[100][100] = {0};
+    int totalnode, node1, node2, weight;
+    int mat[10][10] = {0};
 
     ifstream input("input.txt");
 
     if(input.is_open()){
-        cout<<"File Open\n";
+        cout<<"File open\n";
         input>>totalnode;
 
-        while(input>>node1>>node2>>weight){
+
+    while(input>>node1>>node2>>weight){
             mat[node1][node2] = weight;
-        }
+    }
 
-        ofstream output("Output.txt");
+    ofstream output("output.txt");
 
-        if(output.is_open()){
-                cout<<"File Created\n";
+    if(output.is_open()){
+        cout<<"File Created\n";
+
         for(int i = 1; i<=totalnode; i++){
-            for(int j = 1; j<=totalnode; j++)
-                  output<<mat[i][j]<<" ";
-                      output<<endl;
-
-            }
-
+            for (int j = 1; j<totalnode; j++)
+                output<<mat[i][j]<<" ";
+                output<<endl;
         }
-    } else
-
-    cout<<"File not found";
+    }
+    else{
+        cout<<"File not found";
+    }
 
     return 0;
-
-
+    }
 }
